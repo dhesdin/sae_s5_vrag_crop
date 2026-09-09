887ffc7bb6d6c3fe08c533023a2e390b0aab6be9
karadocDebian <frederic.gobfert@gmail.com>
2026-09-09 22:06:56 +0200

    feat: add BoundingBox and VisualElement classes with pydantic BaseModel

---
+++ src/v_crop_rag/models/vlm.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field
+
+class BoundingBox(BaseModel):
+    x_min: int # A MODIFIER SELON CONFIG VALENTIN (ici coord pixel mais possible aussi de coordonnées normalisées)
+    y_min: int
+    x_max: int
+    y_max: int
+
+
+class VisualElement(BaseModel):
+    label: str
+    description: str
+    attributes: dict[str, str] = Field(default_factory=dict)
+    bbox: BoundingBox | None = None
+
