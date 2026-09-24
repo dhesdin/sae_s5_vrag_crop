fcf7565971cf7187b3bfb47aad9b200b57cdbad7
Frederic Gobfert <frederic.gobfert@gmail.com>
2026-09-24 20:50:27 +0200

    test: add test parameters of function add

--- tests/test_chroma.py
+++ tests/test_chroma.py
@@ -84,6 +84,8 @@ def test_add_reject_invalid_id_type(invalid_id):
         
         with pytest.raises(TypeError):
             index.add(id=invalid_id, vector=[0.1, -0.2, 0.7], metadata={"label": "test"})
+        
+        mock_collection.add.assert_not_called()
 
 
 
@@ -106,3 +108,79 @@ def test_add_reject_invalid_id_value(invalid_value_id):
         
         with pytest.raises(ValueError):
             index.add(id=invalid_value_id, vector=[0.1, -0.2, 0.7], metadata={"label": "test"})
+        
+        mock_collection.add.assert_not_called()
+        
+
+@pytest.mark.parametrize(
+    "invalid_vector",
+    [
+        "abc",
+        (0.1, 0.2),
+        123,
+        None
+    ]
+)
+def test_add_reject_invalid_vector_type(invalid_vector):
+    with patch("v_crop_rag.storage.chroma.PersistentClient") as mock_persistent_client:
+        index = ChromaVectorIndex(path="chroma/data", collection_name="test_images")
+        
+        mock_client = mock_persistent_client.return_value
+        mock_collection = mock_client.get_or_create_collection.return_value
+        
+        with pytest.raises(TypeError):
+            index.add(id="_object_01", vector=invalid_vector, metadata={"label": "test"})
+        mock_collection.add.assert_not_called()
+
+
+def test_add_reject_empty_vector():
+    with patch("v_crop_rag.storage.chroma.PersistentClient") as mock_persistent_client:
+        index = ChromaVectorIndex(path="chroma/data", collection_name="iamges_test")
+        
+        mock_client = mock_persistent_client.return_value
+        mock_collection = mock_client.add.get_or_create_collection.return_value
+        
+        with pytest.raises(ValueError):
+            index.add(id="test_object_01", vector=[], metadata={"label": "test"})
+        mock_collection.add.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    "invalid_vector",
+    [
+        [1.3, "test", 0.7],
+        [None, 0.2],
+        [True, 0.7],
+    ]
+)
+def test_add_reject_invalid_vector_value(invalid_vector):
+    with patch("v_crop_rag.storage.chroma.PersistentClient") as mock_persitent_client:
+        index = ChromaVectorIndex(path="chroma/data", collection_name="images_test")
+
+        mock_client = mock_persitent_client.return_value
+        mock_collection = mock_client.add.return_value
+        
+        with pytest.raises(TypeError):
+            index.add(id="test_object_01", vector=invalid_vector, metadata={"label": "test"})
+        mock_collection.add.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    "invalid_metadata",
+    [   
+        [],
+        123,
+        None,
+        "test"
+    ]
+)
+def test_add_reject_invalid_metadata_type(invalid_metadata):
+    with patch("v_crop_rag.storage.chroma.PersistentClient") as mock_persistent_client:
+        index = ChromaVectorIndex(path="chroma/data", collection_name="images_test")
+        
+        mock_client = mock_persistent_client.return_value
+        mock_collection = mock_client.add.return_value
+        
+        with pytest.raises(TypeError):
+            index.add(id="test_object_01", vector=[0.1, -0.2, 0.7], metadata=invalid_metadata)
+        mock_collection.add.assert_not_called()
